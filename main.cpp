08acca7893338faa85d603e6f07e4f20f847bc47
Harshita-Batra-1 <harshita0587.be21@chitkara.edu.in>
2022-05-22 00:14:36 +0530

    Initial

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "Hello's" << endl;
+    cout << "Hello\"s coder\"s "<< endl;
+    cout << "'Hello'" << endl;
+    cout << '\"Hello\"'<< endl;  //this is wrong only doubles are used(in python-single can be)
+    cout << "\"Hello world\""<< endl;
+    return 0;
+}
